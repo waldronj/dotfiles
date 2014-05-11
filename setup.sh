cbb0518d5462ef7afb3e695c9005426632fa786a
Josh Waldron <jwaldron@thejwal.com>
2014-05-10 23:50:37 -0500

    Fixed background bleeding issues with vim / tmux

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+dir=$(pwd)
 cd .vim/bundle/
 git clone https://github.com/gmarik/vundle.git
+
+cd ~/
+ln -s $dir/.vimrc .vimrc
+ln -s $dir/.vimrc.after .vimrc.after
+ln -s $dir/.vim .vim
+ln -s $dir/.tmux.conf .tmux.conf
